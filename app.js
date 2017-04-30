3f91d23ae684e6c102b1ea88fdd5463cddc10d3c
fortysixntwo <dennis.petta@gmail.com>
2017-04-30 15:12:04 +0200

    added users.js, global,js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(function(req,res,next){
 });
 
 app.use('/', index);
-app.use('/users/userlist', users);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
