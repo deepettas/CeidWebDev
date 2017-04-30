6b9c4d916fa6a519814cdf819d808b1b72b762b5
fortysixntwo <dennis.petta@gmail.com>
2017-04-30 14:43:49 +0200

    added users.js, global,js

--- app.js
+++ app.js
@@ -46,16 +46,28 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
-
 
 module.exports = app;
