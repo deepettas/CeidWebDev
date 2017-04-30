152e7fb3c3f78568991d4ce7b1a09f11ecfe1460
nomaterials <nomaterials@gmail.com>
2017-04-30 15:54:08 +0300

    correct collection name in user route

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,10 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/userlist', function(req, res) {
     var db = req.db;
-    var collection = db.get('userlist');
+    var collection = db.get('usercollection');
     collection.find({},{},function(e,docs){
         res.json(docs);
     });
 });
+
 module.exports = router;
