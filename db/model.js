2ddb4d44dbc44b9e047073451aaea05acaaa8c28
fortysixntwo <dennis.petta@gmail.com>
2017-05-08 14:35:34 +0200

    Db: Added Schemas

--- db/model.js
+++ db/model.js
@@ -4,7 +4,7 @@
 
 var mongoose = require('mongoose');
 
-
+// Schema [ Model ] ~= Layout for a model
 
 // Schema/Model Initialization
 
@@ -13,8 +13,62 @@ var adminSchema = mongoose.Schema({
     password: String
 });
 var Admin = mongoose.model("Admin", adminSchema);
+//----------------------------------------------------------------
 
-// Create instance of admin
+// Employee
+var EmployeeSchema = mongoose.Schema({
+
+});
+var Employee = mongoose.model("Employee",EmployeeSchema)
+//----------------------------------------------------------------
+
+
+// Local Store
+var LocalStoreSchema = mongoose.Schema({
+
+});
+var LocalStore = mongoose.model("LocalStore",LocalStoreSchema)
+//----------------------------------------------------------------
+
+
+//Transit Hub
+var TransitHubSchema = mongoose.Schema({
+
+});
+var TransitHub = mongoose.model("TransitHub",TransitHubSchema)
+//----------------------------------------------------------------
+
+//Sender
+var SenderSchema = mongoose.Schema({
+
+});
+var Sender = mongoose.model("Sender",SenderSchema)
+//----------------------------------------------------------------
+
+//Recipient
+var RecipientSchema = mongoose.Schema({
+
+});
+var Recipient = mongoose.model("Recipient",RecipientSchema)
+//----------------------------------------------------------------
+
+//Website
+var WebsiteSchema = mongoose.Schema({
+
+});
+var Website = mongoose.model("Website",WebsiteSchema)
+//----------------------------------------------------------------
+//AdminControlPanel
+var AdminControlPanelSchema = mongoose.Schema({
+
+});
+var AdminControlPanel = mongoose.model("AdminControlPanel",AdminControlPanelSchema)
+//----------------------------------------------------------------
+
+
+
+
+// Create instance of admin # Testing
 var admin1 = new Admin({ username: 'JoBell2312',
                          password: 'hashmehashme'});
 // Save to MongoDB
