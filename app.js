1d68dedb0ea5087ef2dcef6c3be44ed202a4ba7b
nomaterials <nomaterials@gmail.com>
2017-05-31 13:58:40 +0300

    Admin Signup route & view

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser'); // Gia ta cookies
+var session = require('express-session');  //for session handling
 var bodyParser = require('body-parser'); //pairnei to body apo html request
 var multer = require('multer');
 var upload = multer();
@@ -19,7 +20,7 @@ var app = express();
 
 // view engine setup (PUG/Jade)
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // STARTUP MIDDLEWARE
 // uncomment after placing your favicon in /public
@@ -30,12 +31,39 @@ app.use(bodyParser.urlencoded({ extended: false }));  // for parsing application
 app.use(cookieParser());  // for parsing cookies attached to the client request object
 app.use(upload.array());  // for parsing multipart/form-data
 app.use(express.static(path.join(__dirname, 'public')));  //enable static file serving
+app.use(session({secret: "Your secret key"}));
 
 // Make our db accessible to our router
 app.use(function(req,res,next){
     req.db = db;
     next();
 });
+
+
+var Admins = [];
+
+//Show admin signup view
+app.get('/signupAdmin', function(req, res) {
+    res.render('signupAdmin');
+});
+
+app.post('/signupAdmin', function(req, res) {
+    if( !req.body.id || !req.body.password ) {
+        res.status("400");
+        res.send("Invalid details");
+    } else {
+        Admins.filter(function(admin) {
+            if( admin.id == req.body.id ) {
+                res.render('signup', {message: "Admin already exists! Login or choose another admin id"});
+            }
+        });
+        var newAdmin = {id: req.body.id, password: req.body.password};
+        Admins.push(newAdmin);
+        req.session.admin = newAdmin;
+        res.redirect('/adminPanel');
+    }
+});
+
 
 // ROUTER LINKING
 app.use('/', index);
