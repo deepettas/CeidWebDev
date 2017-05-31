54b01eb432847c3b58b2867073d80b0356707956
nomaterials <nomaterials@gmail.com>
2017-05-31 17:10:26 +0300

    route file for admin, also deleted some old tut files

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var multer = require('multer');
 var upload = multer();
 var mongodb = require('mongodb');
 var index = require('./routes/index');
+var admin = require('./routes/admin');
 var users = require('./routes/users');
 
 //Db Configuration & init
@@ -40,89 +41,18 @@ app.use(function(req,res,next){
 });
 
 
-// THIS IS TEMP!!! SHOULD BE STORED IN DB!!!!
-var Admins = [];
-
-// Show admin signup view
-app.get('/signupAdmin', function(req, res) {
-    res.render('signupAdmin');
-});
-
-// Admin Signup method
-app.post('/signupAdmin', function(req, res) {
-    if( !req.body.id || !req.body.password ) {
-        res.status("400");
-        res.send("Invalid details");
-    } else {
-        Admins.filter(function(admin) {
-            if( admin.id == req.body.id ) {
-                res.render('signupAdmin', {message: "Admin already exists! Login or choose another admin id"});
-            }
-        });
-        var newAdmin = {id: req.body.id, password: req.body.password};
-        Admins.push(newAdmin);
-        req.session.admin = newAdmin;
-        res.redirect('/adminPanel');
-    }
-});
-
-function checkSignIn(req, res, next) {
-    if( req.session.admin ) {  // if session exists, proceed to next page
-        next();
-    } else {
-        var err = new Error("Not logged in!");
-        console.log(req.session.admin);
-        next(err);  // error trying to access unauthorized page!!! INTRUDER ALERT
-    }
-}
-
-// Show Admin Panel
-app.get('/adminPanel', checkSignIn, function(req, res) {
-    res.render('adminPanel', {id: req.session.admin.id});  // render view passing user id stored in session
-});
-
-
-// Show Admin Login View
-app.get('/loginAdmin', function (req, res) {
-    res.render('loginAdmin');
-});
-
-// Admin Login method
-app.post('/loginAdmin', function (req, res) {
-    console.log(Admins);
-    if( !req.body.id || !req.body.password ) {
-        res.render('loginAdmin', {message: "Please enter both id and password"});
-    } else {
-        Admins.filter(function(admin) {
-            if( admin.id == req.body.id && admin.password == req.body.password ) {
-                req.session.admin = admin;
-                res.redirect('/adminPanel');
-            }
-        });
-        res.render('loginAdmin', {message: "Invalid Credentials"});
-    }
-});
-
-// Admin Logout route
-app.get('/logoutAdmin', function(req, res) {
-    req.session.destroy(function() {  // destroy session linked to that admin
-        console.log("Admin logged out");
-    });
-    res.redirect('/loginAdmin');  // nav back to Admin login page
-});
-
-// Middleware for handling unauthorised admin access attempts
-app.use('/adminPanel', function(err, req, res, next) {
-    console.log(err);
-    res.redirect('/loginAdmin');  //admin should be authenticated, nav him back to login
-});
-
-
 
 
 // ROUTER LINKING
 app.use('/', index);
+app.use('/admin', admin);
 app.use('/users', users);
+
+
+
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/admin.js
@@ -0,0 +1,90 @@
+/**
+ * Created by nomaterials on 31/05/2017.
+ */
+
+var express = require('express');
+var router = express.Router();
+
+
+// THIS IS TEMP!!! SHOULD BE STORED IN DB!!!!
+var Admins = [];
+
+
+// Show admin signup view
+router.get('/signup', function(req, res) {
+    res.render('signupAdmin');
+});
+
+// Admin Signup method
+router.post('/signup', function(req, res) {
+    if( !req.body.id || !req.body.password ) {
+        res.status("400");
+        res.send("Invalid details");
+    } else {
+        Admins.filter(function(admin) {
+            if( admin.id == req.body.id ) {
+                res.render('signupAdmin', {message: "Admin already exists! Login or choose another admin id"});
+            }
+        });
+        var newAdmin = {id: req.body.id, password: req.body.password};
+        Admins.push(newAdmin);
+        req.session.admin = newAdmin;
+        res.redirect('/admin/panel');
+    }
+});
+
+function checkSignIn(req, res, next) {
+    if( req.session.admin ) {  // if session exists, proceed to next page
+        next();
+    } else {
+        var err = new Error("Not logged in!");
+        console.log(req.session.admin);
+        next(err);  // error trying to access unauthorized page!!! INTRUDER ALERT
+    }
+}
+
+// Show Admin Panel
+router.get('/panel', checkSignIn, function(req, res) {
+    res.render('adminPanel', {id: req.session.admin.id});  // render view passing user id stored in session
+});
+
+
+// Show Admin Login View
+router.get('/login', function (req, res) {
+    res.render('loginAdmin');
+});
+
+// Admin Login method
+router.post('/login', function (req, res) {
+    console.log(Admins);
+    if( !req.body.id || !req.body.password ) {
+        res.render('loginAdmin', {message: "Please enter both id and password"});
+    } else {
+        Admins.filter(function(admin) {
+            if( admin.id == req.body.id && admin.password == req.body.password ) {
+                req.session.admin = admin;
+                res.redirect('/admin/panel');
+            }
+        });
+        res.render('loginAdmin', {message: "Invalid Credentials"});
+    }
+});
+
+// Admin Logout route
+router.get('/logout', function(req, res) {
+    req.session.destroy(function() {  // destroy session linked to that admin
+        console.log("Admin logged out");
+    });
+    res.redirect('/admin/login');  // nav back to Admin login page
+});
+
+// Middleware for handling unauthorised admin access attempts
+router.use('/panel', function(err, req, res, next) {
+    console.log(err);
+    res.redirect('/admin/login');  //admin should be authenticated, nav him back to login
+});
+
+
+
+//export this router to use in our app.js
+module.exports = router;
