0393bce04801d27038feb126e1e3cb7e81cfdecc
fortysixntwo <dennis.petta@gmail.com>
2017-06-04 18:35:27 +0300

    Updated Local Store

--- db/models/LocalStore.js
+++ db/models/LocalStore.js
@@ -9,6 +9,28 @@ var Schema = mongoose.Schema;
 
 var LocalStoreSchema = Schema({
 
+    transit_hub_id: {type: Schema.ObjectId, ref: 'TransitHub', required: true},
+    phone_number: {type: Number, required: true},
+
+    Address: [{
+        City: {type: String, required: true},
+        Street: {type: String, required: true},
+        Number: {type: String, required: true},
+        Post_code: {type: Number, required: true}
+    }],
+
+
+    Location: [{
+        Longitude: {type: String},
+        Latitude: {type: String}
+    }],
+
+    // All stored packages in the current Store
+    Stored_packages: [{
+        package_id: { type: Schema.ObjectId, ref:'Package', unique: true}
+
+    }]
+
 });
 
 
