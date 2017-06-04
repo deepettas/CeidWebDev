6284e6ce6669d4472fc0820b05524afdfc2aed93
fortysixntwo <dennis.petta@gmail.com>
2017-06-04 16:05:22 +0300

    Updated TransitHub model

--- db/models/TransitHub.js
+++ db/models/TransitHub.js
@@ -7,9 +7,6 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 
-// Used for password hashing
-var bcrypt = require('bcryptjs');
-var SALT_WORK_FACTOR = 10;
 
 
 var TransitHubSchema = Schema({
@@ -19,40 +16,5 @@ var TransitHubSchema = Schema({
     //local_store_id: {type: Schema.ObjectId, required: true, unique: true}
 
 });
-
-// Password hashing BEFORE save to DB
-TransitHubSchema.pre('save', function(next) {
-    var transit = this;
-
-    // only hash the password if it has been modified (or is new)
-    if (!transit.isModified('password'))
-        return next();
-
-    // generate a salt
-    bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
-        if (err)
-            return next(err);
-
-        // hash the password using our new salt
-        bcrypt.hash(transit.password, salt, function (err, hash) {
-            if (err)
-                return next(err);
-
-            // override the cleartext password with the hashed one
-            transit.password = hash;
-            next();
-
-        });
-    });
-});
-
-TransitHubSchema.methods.comparePassword =  function(candidatePassword, cb) {
-    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-       if (err)
-           return cb(err);
-       cb(null, isMatch);
-    });
-
-};
 
 module.exports = mongoose.model("TransitHub", TransitHubSchema);
