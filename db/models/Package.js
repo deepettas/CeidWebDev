dbb85af63241cbadcb55c291dd6478142c798793
fortysixntwo <dennis.petta@gmail.com>
2017-06-04 18:16:00 +0300

    Updated Package final #believe

--- db/models/Package.js
+++ db/models/Package.js
@@ -8,18 +8,25 @@ var Schema = mongoose.Schema;
 
 
 var PackageSchema = Schema({
+    // Schema connections
     sender_id: {type: Schema.ObjectId, ref: 'Sender', required: true},
     recipient_id: {type: Schema.ObjectId, ref: 'Recipient', required: true},
     transit_hub_id: {type: Schema.ObjectId, ref: 'TransitHub', required: true},
-    exrpess: {type: Boolean, required: true},
-    qr_code: {type: kati, required: true},
-    delivery_address: {type: String, required: true},
 
+    exrpess: {type: Boolean, required: true},
+    qr_code: {type: kati, required: true},  //TODO: Add type
+    delivery_address: {type: String, required: true},
+    cost: {type: Number, required: true},
+
+
+    // Current location of the package
     current_location: [{
         Longitude: {type: String},
         Latitude: {type: String}
     }],
 
+    // Array that holds all the previous of space-time
+    // combinations of the package
     locations_passed :  [
         {
             Date: {type: Date},
@@ -28,8 +35,6 @@ var PackageSchema = Schema({
                 Latitude: {type: String}
             }]
         }],
-
-    cost: {type: Number, required: true}
 
 
 });
